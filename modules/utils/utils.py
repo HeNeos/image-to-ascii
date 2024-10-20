4f1b5b7f5a90333c0009af9568b73b0722dcc921
HeNeos <heneos3.14@gmail.com>
2024-10-20 23:47:00 +0200

    Update font

--- modules/utils/utils.py
+++ modules/utils/utils.py
@@ -1,11 +1,14 @@
 import cairo
 import numpy as np
+import ctypes as ct
 
 from typing import List
 from modules.ascii_dict import AsciiDict
 from modules.utils.font import Font
 from modules.utils.custom_types import AsciiColors, AsciiImage
 from PIL import Image
+
+_initialized = False
 
 
 def create_char_array(ascii_dict: AsciiDict) -> np.ndarray:
@@ -21,6 +24,98 @@ def map_to_char(gray_scale: float, ascii_dict: AsciiDict) -> str:
     return ascii_dict.value[position]
 
 
+def create_cairo_font_face_for_file(filename, faceindex=0, loadoptions=0):
+    global _initialized
+    global _freetype_so
+    global _cairo_so
+    global _ft_lib
+    global _ft_destroy_key
+    global _surface
+
+    CAIRO_STATUS_SUCCESS = 0
+    FT_Err_Ok = 0
+
+    if not _initialized:
+        _freetype_so = ct.CDLL("libfreetype.so.6")
+        _cairo_so = ct.CDLL("libcairo.so.2")
+        _cairo_so.cairo_ft_font_face_create_for_ft_face.restype = ct.c_void_p
+        _cairo_so.cairo_ft_font_face_create_for_ft_face.argtypes = [
+            ct.c_void_p,
+            ct.c_int,
+        ]
+        _cairo_so.cairo_font_face_get_user_data.restype = ct.c_void_p
+        _cairo_so.cairo_font_face_get_user_data.argtypes = (ct.c_void_p, ct.c_void_p)
+        _cairo_so.cairo_font_face_set_user_data.argtypes = (
+            ct.c_void_p,
+            ct.c_void_p,
+            ct.c_void_p,
+            ct.c_void_p,
+        )
+        _cairo_so.cairo_set_font_face.argtypes = [ct.c_void_p, ct.c_void_p]
+        _cairo_so.cairo_font_face_status.argtypes = [ct.c_void_p]
+        _cairo_so.cairo_font_face_destroy.argtypes = (ct.c_void_p,)
+        _cairo_so.cairo_status.argtypes = [ct.c_void_p]
+        _ft_lib = ct.c_void_p()
+        status = _freetype_so.FT_Init_FreeType(ct.byref(_ft_lib))
+        if status != FT_Err_Ok:
+            raise RuntimeError("Error %d initializing FreeType library." % status)
+
+        class PycairoContext(ct.Structure):
+            _fields_ = [
+                ("PyObject_HEAD", ct.c_byte * object.__basicsize__),
+                ("ctx", ct.c_void_p),
+                ("base", ct.c_void_p),
+            ]
+
+        _surface = cairo.ImageSurface(cairo.FORMAT_A8, 0, 0)
+        _ft_destroy_key = ct.c_int()
+        _initialized = True
+
+    ft_face = ct.c_void_p()
+    cr_face = None
+    try:
+        status = _freetype_so.FT_New_Face(
+            _ft_lib, filename.encode("utf-8"), faceindex, ct.byref(ft_face)
+        )
+        if status != FT_Err_Ok:
+            raise RuntimeError(
+                "Error %d creating FreeType font face for %s" % (status, filename)
+            )
+        cr_face = _cairo_so.cairo_ft_font_face_create_for_ft_face(ft_face, loadoptions)
+        status = _cairo_so.cairo_font_face_status(cr_face)
+        if status != CAIRO_STATUS_SUCCESS:
+            raise RuntimeError(
+                "Error %d creating cairo font face for %s" % (status, filename)
+            )
+        if (
+            _cairo_so.cairo_font_face_get_user_data(cr_face, ct.byref(_ft_destroy_key))
+            is None
+        ):
+            status = _cairo_so.cairo_font_face_set_user_data(
+                cr_face, ct.byref(_ft_destroy_key), ft_face, _freetype_so.FT_Done_Face
+            )
+            if status != CAIRO_STATUS_SUCCESS:
+                raise RuntimeError(
+                    "Error %d doing user_data dance for %s" % (status, filename)
+                )
+            ft_face = None
+        cairo_ctx = cairo.Context(_surface)
+        cairo_t = PycairoContext.from_address(id(cairo_ctx)).ctx
+        _cairo_so.cairo_set_font_face(cairo_t, cr_face)
+        status = _cairo_so.cairo_font_face_status(cairo_t)
+        if status != CAIRO_STATUS_SUCCESS:
+            raise RuntimeError(
+                "Error %d creating cairo font face for %s" % (status, filename)
+            )
+
+    finally:
+        _cairo_so.cairo_font_face_destroy(cr_face)
+        _freetype_so.FT_Done_Face(ft_face)
+
+    face = cairo_ctx.get_font_face()
+    return face
+
+
 def create_ascii_image(
     ascii_art: AsciiImage, image_colors: AsciiColors
 ) -> cairo.ImageSurface:
@@ -33,9 +128,8 @@ def create_ascii_image(
     surface = cairo.ImageSurface(cairo.FORMAT_RGB24, surface_width, surface_height)
     context = cairo.Context(surface)
 
-    context.select_font_face(
-        "Monospace", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL
-    )
+    face = create_cairo_font_face_for_file("consolas.ttf", 0)
+    context.set_font_face(face)
     context.set_font_size(12)
 
     y = 0
