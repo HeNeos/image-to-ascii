fa3f02685921c15d11dd4fa2ad5e895707870ed0
HeNeos <heneos3.14@gmail.com>
2025-01-03 16:48:25 +0100

    Cleanup

--- modules/ascii_dict/__init__.py
+++ modules/ascii_dict/__init__.py
@@ -1,14 +1,6 @@
 from typing import TypeAlias
 
-# from dataclasses import dataclass
-
 from .black_and_white import AsciiDictBlackWhite
 from .color import AsciiDictColor
 
 AsciiDict: TypeAlias = AsciiDictBlackWhite | AsciiDictColor
-
-
-# @dataclass
-# class AsciiDict:
-#     black_and_white = AsciiDictBlackWhite
-#     color = AsciiDictColor
