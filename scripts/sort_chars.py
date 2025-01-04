87692013bad3763b7b78c21c4861719b652e3a1f
HeNeos <heneos3.14@gmail.com>
2025-01-04 04:05:04 +0100

    Add sort chars script

---
+++ scripts/sort_chars.py
@@ -0,0 +1,31 @@
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+
+ascii_chars = [chr(i) for i in range(32, 127)]
+
+font_size = 14
+font_path = "./consolas.ttf"
+canvas_size = font_size * 64
+canvas_width = int(canvas_size * 6 / 9)
+font = ImageFont.truetype(font_path, font_size)
+
+char_luminosities = {}
+
+for char in ascii_chars:
+    img = Image.new("L", (canvas_width, canvas_size), color=0)
+    draw = ImageDraw.Draw(img)
+
+    for y in range(0, canvas_size, font_size):
+        for x in range(0, canvas_width, font_size):
+            draw.text((x, y), char, font=font, fill=255)
+
+    pixel_data = np.array(img)
+    luminosity = np.mean(pixel_data)
+
+    char_luminosities[char] = luminosity
+
+sorted_chars = sorted(char_luminosities.items(), key=lambda x: x[1])
+for char, lum in sorted_chars:
+    print(f"'{char}': {lum:.2f}")
+
+print("".join([x[0] for x in sorted_chars]))
