bdbd93df674495b44b2f190d8d86d427614d5320
HeNeos <heneos3.14@gmail.com>
2025-01-19 00:43:24 +0100

    Remove _ as edge

--- modules/utils/utils.py
+++ modules/utils/utils.py
@@ -32,13 +32,13 @@ def create_char_array(ascii_dict: AsciiDict) -> npt.NDArray[np.str_]:
 )
 def map_angle_to_ascii(angle: float) -> int:
     if -22.5 <= angle < 22.5 or 157.5 <= angle <= 180 or -180 <= angle < -157.5:
-        return 0
+        return 0  # |
     elif 67.5 <= angle < 112.5 or -112.5 <= angle < -67.5:
-        return 1
+        return -1  # _
     elif 22.5 <= angle < 67.5 or -157.5 <= angle < -112.5:
-        return 2
+        return 2  # /
     elif 112.5 <= angle < 157.5 or -67.5 <= angle < -22.5:
-        return 3
+        return 3  # \
     else:
         return -1  # No edge
 
