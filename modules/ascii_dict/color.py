0fd76a85cc60dba77bf2f13209319ca865fa06e2
HeNeos <heneos3.14@gmail.com>
2025-01-18 21:51:50 +0100

    Update dict

--- modules/ascii_dict/color.py
+++ modules/ascii_dict/color.py
@@ -7,7 +7,7 @@ class AsciiDictColor(Enum):
         + '"'
         + "~^=;!><+\\/*?crL)7T(|zJsviCltF1}{I3fY[]5nu2xjZoSyeaEPVhkU694KGwbdqOpXHAmRD#08WBM%N$Qg&@"  # noqa: E501
     )
-    HighAsciiDict = " !*i713noah6bdwmD08B@"
-    LowAsciiDict = " !*i1oawhb8B@"
-    # HighAsciiDict = " :!si{35aVU9qOD#8$&@@"
-    # LowAsciiDict = " !loa6O#8&@"
+    # HighAsciiDict = " !*i713noah6bdwmD08B@"
+    # LowAsciiDict = " !*i1oawhb8B@"
+    HighAsciiDict = " :!si{35aVU9qOD#8$&@@"
+    LowAsciiDict = " !loa6O#8&@"
