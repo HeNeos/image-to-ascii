95db1218a55128ebe5de5fa7e23eff0d2f62a56e
HeNeos <heneos3.14@gmail.com>
2025-04-11 01:56:59 +0200

    Fix black and white ascii dict

--- modules/ascii_dict/black_and_white.py
+++ modules/ascii_dict/black_and_white.py
@@ -7,5 +7,5 @@ class AsciiDictBlackWhite(Enum):
         + '"'
         + "~^=;!><+\\/*?crL)7T(|zJsviCltF1}{I3fY[]5nu2xjZoSyeaEPVhkU694KGwbdqOpXHAmRD#08WBM%N$Qg&@"  # noqa: E501
     )
-    HighAsciiDict = "    ``.':;*c7t3eK6ON&@"
+    HighAsciiDict = "  ```.':,;+*c7t13ueK6O#&@"
     LowAsciiDict = "  `.':;*vx5K4&@"
